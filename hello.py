127e85c954bacd29314fd884c4dcb3483499113c
Christian <christian@pop-os.localdomain>
2023-08-26 21:51:19 -0500

    Comienzos con Pyhton

---
+++ hello.py
@@ -0,0 +1,19 @@
+# Esto es un comentario
+# Hola Mundo
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es un comentario
+en varias
+lineas
+"""
+
+'''
+Este es otro
+tipo de comentario
+en varias lineas
+'''
+
+# Consultar el tipo de datos
+print(type("Hola Python")) # Tipo 'str'
