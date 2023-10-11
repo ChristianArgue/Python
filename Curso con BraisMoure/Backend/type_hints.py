78b78a8664423262831b2fd081f03b24f7a2bfde
Christian <christianarguello97@hotmail.com>
2023-10-10 19:56:27 -0500

    Comenzamos aprender backend

---
+++ Curso con BraisMoure/Backend/type_hints.py
@@ -0,0 +1,17 @@
+# Type hints
+
+my_string_variable = 'My String Variable'
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_varibale: str = 'My Typed String variable'
+print(my_typed_varibale)
+print(type(my_typed_varibale))
+
+my_typed_varibale: int = 5
+print(my_typed_varibale)
+print(type(my_typed_varibale))
